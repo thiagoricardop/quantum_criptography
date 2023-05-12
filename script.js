743682104725820263715f820486eb54fc2741af
Thiago Ricardo <t.ricardooliveira08@gmail.com>
2023-05-11 22:31:32 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+     $('.scroll-up-btn').click(function(){
+         $('html').animate({scrollTop: 0});
+     });
+
+     var typed = new Typed(".typing", {
+         strings:["Developer", "Designer", "Freelancer"],
+         typeSpeed:60,
+         backSpeed:60,
+         loop:true
+     });
+
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+    $('.projects .carousel').owlCarousel({
+        margin:20,
+        loop:true,
+        autoplayTimeOut:2000,
+        autoplayHoverPauser:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:false
+            },
+            600:{
+                items:2,
+                nav:false
+            },
+            1000:{
+                items:3,
+                nav:false
+            }
+        }
+    });
+    $('.skills .carousel').owlCarousel({
+      
+      loop: true,
+      autoplayHoverPauser: true,
+      items: 6, //numero de itens visiveis
+      loop: true, //se vai fazer loop
+      margin: 0, //espacamento entre os itens
+      autoplay: true, //se vai ser automatico
+      autoplayTimeout: 2000, //tempo entre as transições 
+  });
+});
